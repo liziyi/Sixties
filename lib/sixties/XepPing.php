a2d99f52f7e2203bdf3c1752545723b053e3fb3b
Clochix <clochix@clochix.net>
2009-08-12 23:10:21 +0000

    Add basic support of XMPP Ping

---
+++ lib/sixties/XepPing.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * This file is part of Sixties, a set of classes extending XMPPHP, the PHP XMPP library from Nathanael C Fritz
+ *
+ * Copyright (C) 2009  Clochix.net
+ *
+ * Sixties is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Sixties is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sixties; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category  Library
+ * @package   Sixties
+ * @author    Clochix <clochix@clochix.net>
+ * @copyright 2009 Clochix.net
+ * @license   http://www.gnu.org/licenses/gpl.txt GPL
+ * @link      https://labo.clochix.net/projects/show/sixties
+ */
+
+require_once dirname(__FILE__) . "/Xep.php";
+
+/**
+ * XepPing : implement part of client-side of XEP 0199 : XMPP Ping
+ *
+ * @category  Library
+ * @package   Sixties
+ * @author    Clochix <clochix@clochix.net>
+ * @copyright 2009 Clochix.net
+ * @license   http://www.gnu.org/licenses/gpl.txt GPL
+ * @version   $Id$
+ * @link      https://labo.clochix.net/projects/show/sixties
+ */
+class XepPing extends Xep
+{
+
+    const EVENT_PONG = 'ping_event_pong';
+
+    /**
+     * Create object and register handlers
+     *
+     * @param XMPP2  $conn the connexion
+     * @param string $host the pubsub server
+     *
+     * @return void
+     */
+    public function __construct($conn, $host = null) {
+        parent::__construct($conn);
+    }
+
+    /**
+     * Is the server alive ?
+     *
+     * @return XepCommand $this
+     */
+    public function ping() {
+        $req = array('msg' => "<ping xmlns='urn:xmpp:ping'/>");
+        $this->conn->addIdHandler($this->conn->getNextId(), 'handlerPong', $this);
+        $this->conn->sendIq($req);
+        return $this;
+    }
+    /**
+     * Handle pong
+     *
+     * @param XMPPHP_XMLObj $xml the response
+     *
+     * @return void
+     */
+    public function handlerPong(XMPPHP_XMLObj $xml){
+        try {
+            $res = $this->commonHandler($xml);
+            if ($res->code != XepResponse::XEPRESPONSE_KO) {
+                $res->message = "Pong";
+                $this->conn->event(self::EVENT_PONG, $res);
+            }
+        } catch (Exception $e) {
+            $res = new XepResponse($e->getMessage(), XepResponse::XEPRESPONSE_KO);
+            $this->conn->event(self::EVENT_ERROR, $res);
+        }
+    }
+
+}
